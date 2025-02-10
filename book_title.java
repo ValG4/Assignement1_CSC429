d44a3a165e97235876ddb66264e1a04610b23ade
Val <valentin.goyet3@gmail.com>
2025-02-10 19:10:25 +0100

    added year for books and patrons (need revision)

--- book_title.java
+++ book_title.java
@@ -8,7 +8,7 @@ public class BookCollection{
         list_books = new Vector<>();
 
     }
-    public void book_find(String title) throws SQLExeption{
+    public void book_find_title(String title) throws SQLExeption{
         list_books.clear();
         String query = "SELECT * FROM Book WHERE bookTitle LIKE" + title;
         try(){
@@ -16,7 +16,24 @@ public class BookCollection{
         }
 
     }
-    Public void results_display(){
+
+    public void book_find_year(int year) throws SQLException{
+        list_books.clear();
+        String query = "SELECT * FROM Book WHERE pubYear <=" + year;
+        try(){
+            result = query.executeQuery();
+        }
+    }
+
+    public void patron_age(int date) throws SQLException{
+        list_books.clear();
+        String query = "SELECT Name FROM Patron WHERE dateOfBirth <=" + date;
+        try(){
+            result = query.executeQuery();
+        }
+    }
+
+    public void results_display(){
         if (list_books.isEmpty()){
             System.out.println("No books matching in the Database");
 
@@ -25,3 +42,7 @@ public class BookCollection{
         }
     }
 }
+
+Given a year, print all book data for books that are published before that year.
+5. Given a date, print all patron data for patrons that are younger than that date.
+6. Given a zip, print all patron data for patrons that live at that zip
